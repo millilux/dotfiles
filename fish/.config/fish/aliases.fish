2ece92983785aaa72bd45405f7514b2644833c83
millilux <millilux@users.noreply.github.com>
2023-08-04 21:26:49 +0100

    Update fish aliases

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -4,11 +4,12 @@ alias vimdiff 'nvim -d'
 alias g "git"
 alias ga "git add"
 alias gs "git status"
-alias gd "git difftool" 
+alias gd "git diff" 
 alias gc "git commit" 
 alias gco "git checkout" 
 alias gr "git rebase"
 alias gl "git log --oneline --decorate --graph --all"
+alias gL "git log --decorate --graph --all"
 
 alias ls "exa --tree --level=1 --icons"
 alias ll "exa --long --tree -g --level=1 --icons"
