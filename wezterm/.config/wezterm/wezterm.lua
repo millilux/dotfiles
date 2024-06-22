78ab1606e4610a25423981b7fcf8c2019359ed6b
millilux <millilux@users.noreply.github.com>
2024-06-22 18:44:30 +0100

    Tidy

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -95,19 +95,19 @@ return {
             key = 'r',
             mods = 'LEADER',
             action = act.PromptInputLine {
-            description = 'Enter new name for tab',
-            action = wezterm.action_callback(function(window, pane, line)
-                -- line will be `nil` if they hit escape without entering anything
-                -- An empty string if they just hit enter
-                -- Or the actual line of text they wrote
-                if line then
-                window:active_tab():set_title(line)
-                end
-            end),
+                description = 'Enter new name for tab',
+                action = wezterm.action_callback(function(window, pane, line)
+                    -- line will be `nil` if they hit escape without entering anything
+                    -- An empty string if they just hit enter
+                    -- Or the actual line of text they wrote
+                    if line then
+                        window:active_tab():set_title(line)
+                    end
+                end),
             },
         },
         -- Fix Arrow Keys within WSL
-        {key='UpArrow', action=wezterm.action{SendString='\x1b[A'}},
-        {key='DownArrow', action=wezterm.action{SendString='\x1b[B'}},
+        { key = 'UpArrow', action = wezterm.action { SendString = '\x1b[A' } },
+        { key = 'DownArrow', action = wezterm.action { SendString = '\x1b[B' } },
     }
 }
